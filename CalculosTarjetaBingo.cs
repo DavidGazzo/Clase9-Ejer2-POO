88c486614dbb2625ee89ee46b76747fa8b86729d
DavidGazzo <dagazzo@gmail.com>
2022-06-03 01:33:43 -0300

    Agregar archivos de proyecto.

---
+++ CalculosTarjetaBingo.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase9_Ejer2_POO
+{
+    public class CardOperations     // Genera los numeros para cada tarjeta
+                                    // con 9 columnas, 1 ó 2 nros por column,5 números por row,
+                                    // los números no se repiten y están ordenados en cadaa column por decena
+                                    // y si si¡on dos nros en la column el menor va en row superior
+    {
+        public static int[,] Main()
+        {
+            int[,] CartonBingo = new int[3,9];
+            //bool generateMatriz = true;
+            int[] numros = NumberXColumn();
+            GeneraNummbers(ref CartonBingo,numros);
+                        
+            return CartonBingo;
+        }   // End Main
+        
+        public static int[] NumberXColumn()
+        {   
+            // Devuelve un vector con la cant de nros por column en un total de 15 nros
+            int addition = 0;
+            var evenOdd = new Random();
+            int[] vectorNumbersXColumns = new int[9];
+            do      // Itera hasta conseguir una combinación con 15 números
+            {
+                for (int i = 0; i < vectorNumbersXColumns.Length; i++)
+                {
+                    vectorNumbersXColumns[i] = evenOdd.Next(1, 3);   // Genera 2 ó 1 nro por column
+                }
+                addition = 0;
+                foreach (var item in vectorNumbersXColumns)
+                {
+                    addition += item;       // Luego de generado el vector addition sus ítems
+                }
+
+            } while (addition != 15);       // Cuando sean 15 sale de bucle
+
+            return vectorNumbersXColumns;
+        }   // End NumberXColumn
+
+        public static int[,] GeneraNummbers(ref int[,] CartonBingo,int[] NumberXColumn)
+        {
+            int contIntentos = 0;
+            bool hacerNuevoCarton = false;
+            // Genera los 15 numeros del carton
+            do
+            {             
+                contIntentos++;
+                Random numeros = new Random();
+                int posicionMatriz = 0;         // para posición en column de matriz
+                int decenaInicio = 1;           // para cálculo de las decenas
+                int decenaFin = 10;             // para cálculo de las decenas
+                int[] columnasAux = new int[3]; // guarda nros aleatorios de 1 column
+
+                // Lugares en column
+                Random random = new Random();
+                int ubicacion = 0;              // Para colocar en 1ra,2da,3ra row
+                int cargarNro = 0;              // Aux para ordenar nros de mayor a menor
+                int cargarNro2 = 0;             // Aux para ordenar nros de mayor a menor
+
+                foreach (var item in NumberXColumn)
+                {
+                    switch (item)
+                    {
+                        case 1:
+                            ubicacion = random.Next(1, 3);
+                            cargarNro = numeros.Next(decenaInicio, decenaFin);  // Genera nro según decena
+                            switch (ubicacion)              // Decide lugar entre 3 filas (para 1 solo nro en la column)
+                            {
+                                case 1:                     //nro,0,0
+                                    columnasAux[0] = cargarNro;
+                                    columnasAux[1] = 00;
+                                    columnasAux[2] = 00;
+                                    break;
+                                case 2:                     //0,nro,0
+                                    columnasAux[0] = 00;
+                                    columnasAux[1] = cargarNro;
+                                    columnasAux[2] = 00;
+                                    break;
+                                case 3:                     //0,0,nro
+                                    columnasAux[0] = 00;
+                                    columnasAux[1] = 00;
+                                    columnasAux[2] = cargarNro;
+                                    break;
+                            }
+                            break;
+
+                        case 2:   // Para 2 nros por column
+                            ubicacion = random.Next(1, 3);
+                            bool nrosIguales = true;
+                            while (nrosIguales)     // Itera generando dos nros, hasta conseguir dos diferentes y ordenados menor y mayor                                    
+                            {
+                                cargarNro = numeros.Next(decenaInicio, decenaFin);  // Genera 1er nro segun decena
+                                cargarNro2 = numeros.Next(decenaInicio, decenaFin); // Genera 2do nro segun decena
+                                if (cargarNro < cargarNro2)     // Solo entra si están ordenados menor-mayor...
+                                {
+                                    nrosIguales = false;        // ...para salir del bucle
+                                }
+                            }
+                            switch (ubicacion)// Decide lugar entre 2 lugares
+                                              // y luego entre los 2 restante si el nro quedo en row 0
+                            {
+                                case 1:     // Ubicar 1er nro en row 1 ó 2, ergo el 2do en row 2 ó 3
+                                    columnasAux[0] = cargarNro; // nro,?,?
+                                    ubicacion = random.Next(1, 3);
+                                    //cargarNro = numeros.Next(decenaInicio, decenaFin);
+                                    if (ubicacion == 1)           // nro,nro,0
+                                    {
+                                        columnasAux[1] = cargarNro2;
+                                        columnasAux[2] = 00;
+                                    }
+                                    else if (ubicacion == 2)    // nro,0,nro
+                                    {
+                                        columnasAux[1] = 00;
+                                        columnasAux[2] = cargarNro2;
+                                    }
+                                    break;
+
+                                case 2:
+                                    ubicacion = random.Next(1, 2);  // 0,nro,nro
+                                                                    //cargarNro = numeros.Next(decenaInicio, decenaFin);
+                                    columnasAux[0] = 0;
+                                    columnasAux[1] = cargarNro;
+                                    //cargarNro = numeros.Next(decenaInicio, decenaFin);
+                                    columnasAux[2] = cargarNro2;
+                                    break;
+                            }
+                            break;
+                    }
+                    for (int filas = 0; filas < 3; filas++)     // Va guardando en "el cartón" cada una de las columnas generadas
+                    {
+                        CartonBingo[filas, posicionMatriz] = columnasAux[filas];
+                    }
+                    posicionMatriz++;               // Avanza en las columnas
+                    decenaFin = decenaFin + 10;      // Prepara indicadores de decena
+                    decenaInicio = decenaFin - 9;
+                }
+                hacerNuevoCarton = CtrlFiveInFile(CartonBingo, contIntentos);
+            } while (hacerNuevoCarton);
+            return CartonBingo;
+        }   // End GeneraNummbers
+
+        public static bool CtrlFiveInFile(int[,] CartonBingo, int contIntentos)
+        {
+            // controlar que sean 5 numeros por fila
+            bool generateMatriz = false;    // True si hay que generar nueva tarjeta (hay más de 5nros en alguna fila)
+            int filas = 0;                  // Para salir si tarjeta OK
+            bool rowOk = true;              // Mientras la fila controlada este bien
+            while (rowOk&&filas<4)          
+            {         
+                for (int row = 0; row < 3; row++)
+                {
+                    filas++;
+                    int counter = 0;
+                    for (int column = 0; column < 9; column++)
+                    {
+                        int value = CartonBingo[row, column];
+                        if (value != 0) counter++;  // Suma 1 si el valor es distinto de 0(cero)
+                    }
+                    if (counter != 5)   // Si no cumple condicion de 5 nros en una fila
+                    {
+                        rowOk = false;          // La fila es incorrecta
+                        generateMatriz = true;  // Debe generarse nueva tarjeta
+                        break;
+                    }                        
+                }                
+            }            
+            return generateMatriz;  // True si hay que generar nueva tarjeta (hay más de 5nros en alguna fila)
+        }   // End CtrlFiveInFile
+
+    }   // End class CardOperations
+}   // End namespace Clase9_Ejer2_POO
---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿/* REQUERIMIENTOS:
+Con los conocimientos vistos hasta ahora en clase realizar un programa que haga lo siguiente:
+Generar un programa que cree un cartón de bingo aleatorio y lo muestre por pantalla
+1)    Cartón de 3 filas por 9 columnas
+2)    El cartón debe tener 15 números y 12 espacios en blanco
+3)    Cada fila debe tener 5 números
+4)    Cada columna debe tener 1 o 2 números
+5)    Ningún número puede repetirse
+6)    La primer columna contiene los números del 1 al 9, la segunda del 10 al 19, la tercera del 20 al 29,
+      así sucesivamente hasta la última columna la cual contiene del 80 al 90
+7)    Mostrar el carton por pantalla
+
+DISEÑO:     Generar un vector con cant de nros que iran en cada columna, iterar hasta conseguir 15 en total.
+            Luego generar nros por columna(decena), guardar en vector aux, ordenar mayor y menor si son dos.
+            Guardar en matriz cartonBingo, ubicarlos (1ra,2da,3ra fila) aleatoriamente.
+            Imprimir cartón
+ */
+// NOTA: Todos los métodos que utilicen el objeto 'Console' se encuentra en ésta clase BingoCardGenerator.
+// En las clases Tools y CardOpertions no se utilizan objetos 'Console'. Ergo podrán ser usadas en aplicaciones gráficas
+
+
+namespace Clase9_Ejer2_POO
+{
+    class BingoCardGenerator
+    {
+        static void Main (string[] args)
+        {
+            var myTools = new Tools();
+            var nuevaTarjeta = new CardOperations();
+            int[,] BingoCard = new int[3, 9]; // Matriz de 3 filas por 9 columnas Cartón de Bingo
+            string fact = "";
+            bool isNumber = false;
+
+            // Desde aca comienza todo el proceso
+            do {
+                Console.Clear();
+                do  // Presento programa y Solicito cantidad de tarjetas que se generarán
+                {                    
+                    Console.WriteLine(myTools.PresentationPoster("        Generador de Bingo        ")); // Muestra cartel inicio
+                    Console.Write("¿Cuántas tarjetas de bingo desea generar?: ");
+                    fact = Console.ReadLine();
+                    isNumber = myTools.ctrlNumber(fact);    // Controla que el dato ingresado sea un caracter válido
+                    if ((isNumber == false) || (int.Parse(fact) == 0))  // No se permiten ni letres, ni 0(cero) ni alfanumérico
+                    {
+                        Console.WriteLine("Debe ingresar un número mayor que 0(cero).\n Presione ENTER para intentar nuevamente...");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                } while ((isNumber != true) || (int.Parse(fact) == 0));
+
+                int cantCards = int.Parse(fact);
+
+                do // Genero números de la matriz BingoCard e imprimo tarjetas
+                {
+                    BingoCard = CardOperations.Main();  // Genero y controlo matriz de la nueva tarjeta
+                    PrintCard(BingoCard);               // Imprimo tarjeta en pantalla
+                    cantCards--;                        // Se descuenta tarjeta generada/impresa hasta cumplir con las tarjetas solicitadas
+                } while (cantCards != 0);
+
+                Console.Write("\n¿Desea generar mas tarjetas de bingo? S/N : ");                
+
+            } while (myTools.OnlyYesNo(Console.ReadLine()));
+        } // End Main
+
+        static void PrintCard(int[,] BingoCard)
+        {
+            // Generar y Mostrar cartón de bingo
+
+            Random color = new Random();        // Para color aleatorio de tarjetas
+            int nroColor = color.Next(1, 7);    // Guarda valor para seleccionar en switch de color
+            Console.WriteLine("\t\t\t╔══╦══╦══╦══╦══╦══╦══╦══╦══╗");
+            for (int fila = 0; fila < 3; fila++)
+            {
+                for (int columna = 0; columna < 9; columna++)
+                {
+                    int valorNro = BingoCard[fila, columna];
+                    if (valorNro < 10)                      // Imprime por filas
+                    {
+                        if (valorNro == 00)
+                        {   // Si es nro 0(cero) imprime cuadro sombreado
+                            if (columna == 0)
+                            {
+                                Console.Write("\t\t\t║");
+                            }
+                            else
+                            {
+                                Console.Write("║");
+                            }
+
+                            switch (nroColor)   // SWITCH DE COLOR
+                            {   // Colores de los cuadros sombreados (espacios sin números)
+                                case 1:
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    break;
+                                case 2:
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    break;
+                                case 3:
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    break;
+                                case 4:
+                                    Console.ForegroundColor = ConsoleColor.Blue;
+                                    break;
+                                case 5:
+                                    Console.ForegroundColor = ConsoleColor.Cyan;
+                                    break;
+                                case 6:
+                                    Console.ForegroundColor = ConsoleColor.Magenta;
+                                    break;
+                            }
+                            Console.Write("██");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.Write($"\t\t\t║0{valorNro}");     // Si es nro de una cifra agrega un 0(cero)
+                        }
+                    }
+                    else
+                    {
+                        Console.Write($"║{BingoCard[fila, columna]}");    // Si es nro de 2 cifras imprime directamente
+                    }
+                }
+                Console.WriteLine("║"); // Barra vertical final para cada fila
+                if (fila < 2)
+                {
+                    Console.WriteLine("\t\t\t╠══╬══╬══╬══╬══╬══╬══╬══╬══╣");
+                }
+                else if (fila == 2)
+                {
+                    Console.WriteLine("\t\t\t╚══╩══╩══╩══╩══╩══╩══╩══╩══╝");
+                }
+            }
+        }// End PrintCard
+
+    }// End class BingoGenerator
+} // End namespace
